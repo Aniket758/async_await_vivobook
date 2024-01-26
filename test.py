f11f0648f3e75ca223229e0d5b69066b6455e2e9
Aniket Sahoo <aniketsahoo758@gmail.com>
2024-01-26 19:33:15 +0530

    main to merge

--- test.py
+++ test.py
@@ -3,3 +3,7 @@ print("2nd")
 print("3rd commit")
 print("4th c") #this will used as amend
 print("after creation of nav branch")
+print("on nav 1st same line on main")
+print("2nd on nav same line on main")
+print("lets try merger 1 same line on main")
+print("nav mer 2 same line on main")
